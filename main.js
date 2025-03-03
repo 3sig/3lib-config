84acea986a00aca753f9f4dd00ed6e4243ec4a42
Aaron Santiago <me@aaron.work>
2025-03-02 21:26:12 -0500

    Add support for filewatching, add ability to get field with default value

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ import minimist from "minimist";
 import smolToml from "smol-toml";
 import fs from "fs";
 import path from "path";
+import chokidar from "chokidar";
 
 let executableName = path.basename(process.argv[1], ".exe");
 let config = {};
 let configChangedListeners = [];
 
-function get() {
-  return config;
+function get(field, defaultValue) {
+  if (field === undefined) {
+    return config;
+  }
+  if (config[field] === undefined) {
+    return defaultValue;
+  }
+  return config[field];
 }
 
 function addListener(callback) {
@@ -24,16 +31,29 @@ function _setConfig(newConfig) {
   _configChanged();
 }
 
+function _watchConfigFile(filename) {
+  function updateConfig() {
+    try {
+        let fileContents = fs.readFileSync(filename, "utf8");
+        let newConfig = smolToml.parse(fileContents);
+        _setConfig(newConfig);
+      } catch (error) {
+        console.error("Error parsing config file:", error);
+      }
+  }
+  let watcher = chokidar.watch(filename, {
+    awaitWriteFinish: true,
+    atomic: true
+  })
+  watcher.on('change', updateConfig);
+
+  updateConfig();
+}
+
 function init() {
   let argv = minimist(process.argv.slice(2));
   if (argv.f) {
-    try {
-      let fileContents = fs.readFileSync(argv.f, "utf8");
-      let newConfig = smolToml.parse(fileContents);
-      _setConfig(newConfig);
-    } catch (error) {
-      console.error("Error parsing config file:", error);
-    }
+    _watchConfigFile(argv.f);
   } else if (argv._.length === 1) {
     try {
       let newConfig = smolToml.parse(decodeURI(argv._[0]));
@@ -43,22 +63,10 @@ function init() {
     }
   }
   else if (fs.existsSync(executableName + ".toml")){
-    try {
-      let fileContents = fs.readFileSync(executableName + ".toml", "utf8");
-      let newConfig = smolToml.parse(fileContents);
-      _setConfig(newConfig);
-    } catch (error) {
-      console.error("Error parsing default config file:", error);
-    }
+    _watchConfigFile(executableName + ".toml");
   }
   else if (fs.existsSync("config.toml")){
-    try {
-      let fileContents = fs.readFileSync("config.toml", "utf8");
-      let newConfig = smolToml.parse(fileContents);
-      _setConfig(newConfig);
-    } catch (error) {
-      console.error("Error parsing default config file:", error);
-    }
+    _watchConfigFile("config.toml");
   }
   else {
     _setConfig({});
--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 import config from './main.js';
 
+config.addListener(() => {
+  console.log("hello: ", config.get("hello", "default_value"));
+});
+
 config.init();
 
-console.log("hello: ", config.get()["hello"]);
+let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+(async () => {
+  while (true) {
+    await sleep(150);
+  }
+})();
