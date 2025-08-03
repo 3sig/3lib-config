790ba2ce6d3ad2ee31ef166af179be074b37a1a6
Aaron Santiago <me@aaron.work>
2025-08-03 17:41:57 -0400

    fix behavior when attempting to access nonexistent config parameters

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import minimist from "minimist";
-import * as fleece from 'golden-fleece';
+import * as fleece from "golden-fleece";
 import fs from "fs";
 import path from "path";
 import chokidar from "chokidar";
@@ -8,19 +8,23 @@ let config = {};
 let configChangedListeners = [];
 
 function _get(c, field, defaultValue) {
+  if (c === undefined) {
+    return defaultValue
+  }
+
   if (field === undefined) {
-      return c;
-    }
+    return c;
+  }
 
-    let slashSplit = field.split("/");
-    if (slashSplit.length > 1) {
-      return _get(c[slashSplit[0]], slashSplit.slice(1).join("/"), defaultValue);
-    }
+  let slashSplit = field.split("/");
+  if (slashSplit.length > 1) {
+    return _get(c[slashSplit[0]], slashSplit.slice(1).join("/"), defaultValue);
+  }
 
-    if (c[field] === undefined) {
-      return defaultValue;
-    }
-    return c[field];
+  if (c[field] === undefined) {
+    return defaultValue;
+  }
+  return c[field];
 }
 
 function get(field, defaultValue) {
@@ -42,19 +46,25 @@ function _setConfig(newConfig) {
 
 function _watchConfigFile(filename) {
   function updateConfig() {
+    let newConfig;
     try {
-        let fileContents = fs.readFileSync(filename, "utf8");
-        let newConfig = fleece.evaluate(fileContents);
-        _setConfig(newConfig);
-      } catch (error) {
-        console.error("Error parsing config file. filename:", filename, "error: ", error);
-      }
+      let fileContents = fs.readFileSync(filename, "utf8");
+      newConfig = fleece.evaluate(fileContents);
+    } catch (error) {
+      console.error(
+        "Error parsing config file. filename:",
+        filename,
+        "error: ",
+        error,
+      );
+    }
+    if (newConfig) _setConfig(newConfig);
   }
   let watcher = chokidar.watch(filename, {
     awaitWriteFinish: true,
-    atomic: true
-  })
-  watcher.on('change', updateConfig);
+    atomic: true,
+  });
+  watcher.on("change", updateConfig);
 
   updateConfig();
 }
@@ -72,14 +82,11 @@ function init(customArgs) {
     } catch (error) {
       console.error("Error parsing config input:", error);
     }
-  }
-  else if (fs.existsSync(executableName + ".json5")){
+  } else if (fs.existsSync(executableName + ".json5")) {
     _watchConfigFile(executableName + ".json5");
-  }
-  else if (fs.existsSync("config.json5")){
+  } else if (fs.existsSync("config.json5")) {
     _watchConfigFile("config.json5");
-  }
-  else {
+  } else {
     _setConfig({});
   }
 }
